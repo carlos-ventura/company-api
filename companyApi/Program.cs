73ef1920182ebbf7416ebfe9fb40a46e97776a31
carlos-ventura <carlospinho_pt@hotmail.com>
2023-02-15 20:12:18 +0100

    Create simple endpoints for employees

--- companyApi/Program.cs
+++ companyApi/Program.cs
@@ -5,6 +5,20 @@ builder.Services.AddDbContext<EmployeeContext>(opt => opt.UseInMemoryDatabase("E
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/employees", async (EmployeeContext db) =>
+    await db.Employees.ToListAsync());
+
+app.MapPost("/employee", async (Employee employee, EmployeeContext db) =>
+{
+    db.Employees.Add(employee);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/employee/{employee.Id}", employee);
+});
+
+app.MapGet("/employee", async (int employeeId, EmployeeContext db) =>
+{
+    await db.Employees.FirstAsync(e => e.Id == employeeId);
+});
 
 app.Run();
