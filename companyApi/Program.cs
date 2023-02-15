d592f30458ec5874e38de37f487d4ba07a9f038e
carlos-ventura <carlospinho_pt@hotmail.com>
2023-02-15 15:33:48 +0100

    Add database context to main program

--- companyApi/Program.cs
+++ companyApi/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<EmployeeDb>(opt => opt.UseInMemoryDatabase("EmployeeList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
